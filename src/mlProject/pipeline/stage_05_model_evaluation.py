e38221fd2617c33ceae2830a4a4e83c13d8c006b
Sai Ruthvik <msairuthvik@gmail.com>
2025-07-06 14:49:46 -0500

    model evaluation and ml flow added

---
+++ src/mlProject/pipeline/stage_05_model_evaluation.py
@@ -0,0 +1,25 @@
+from mlProject.config.configuration import ConfigurationManager
+from mlProject.components.model_evaluation import ModelEvaluation
+from mlProject import logger
+
+STAGE_NAME = "Model evaluation stage"
+
+class ModelEvaluationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config_manager = ConfigurationManager()
+        model_eval_config = config_manager.get_model_evaluation_config()
+        model_evaluator = ModelEvaluation(config=model_eval_config)
+        model_evaluator.log_into_mlflow()
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelEvaluationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
